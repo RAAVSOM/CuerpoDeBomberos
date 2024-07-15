e8b36e8dbc8a85fa26f197d16a6b9cbaaca17dcd
RAAVSOM <alejo802.s.o@gmail.com>
2024-07-15 13:26:57 -0500

    alejandro

--- Cuerpo_Bomberos/src/app/quienes/quienes.component.ts
+++ Cuerpo_Bomberos/src/app/quienes/quienes.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { EncabezadoComponent } from '../encabezado/encabezado.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-quienes',
   standalone: true,
-  imports: [EncabezadoComponent],
+  imports: [EncabezadoComponent, RouterModule],
   templateUrl: './quienes.component.html',
   styleUrl: './quienes.component.css'
 })
