ef41f268ef8955276715a0bad47bbf74f4c0772b
RAAVSOM <alejo802.s.o@gmail.com>
2024-08-12 10:15:55 -0500

    Alejandro

--- Cuerpo_Bomberos/src/app/app.routes.ts
+++ Cuerpo_Bomberos/src/app/app.routes.ts
@@ -13,6 +13,7 @@ import { BomberitosComponent } from './bomberitos/bomberitos.component';
 import { MapaComponent } from './mapa/mapa.component';
 import { PrevencionComponent } from './prevencion/prevencion.component';
 import { GaleriaBomberitosComponent } from './galeria-bomberitos/galeria-bomberitos.component';
+import { GaleriaComponent } from './galeria/galeria.component';
 
 export const routes: Routes = [
     {path: 'home', component: HomeComponent},
@@ -29,5 +30,6 @@ export const routes: Routes = [
     {path: 'mapa', component: MapaComponent},
     {path: 'GaleriaBomberitos', component: GaleriaBomberitosComponent},
     {path: 'prevencion', component: PrevencionComponent},
+    {path: 'galeria', component: GaleriaComponent},
     {path: '', redirectTo: 'home', pathMatch: 'full'},
 ];
--- Cuerpo_Bomberos/src/app/galeria/galeria.component.ts
+++ Cuerpo_Bomberos/src/app/galeria/galeria.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { EncabezadoComponent } from "../encabezado/encabezado.component";
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-galeria',
   standalone: true,
-  imports: [],
+  imports: [EncabezadoComponent, FooterComponent],
   templateUrl: './galeria.component.html',
   styleUrl: './galeria.component.css'
 })
